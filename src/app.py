177d81d28a4aef80ac93f01970537cb91eea3f7d
sourav dwivedi <souravdwivedi999@gmail.com>
2026-03-16 22:59:26 +0530

    Added minor changes

--- src/app.py
+++ src/app.py
@@ -1,31 +1,41 @@
-from rag_chain import build_rag_chain
+# from rag_chain import build_rag_chain
 
 
-def main():
+# def main():
 
-    print("RAG Document Assistant Started")
-    print("Type 'exit' to quit\n")
+#     print("RAG Document Assistant Started")
+#     print("Type 'exit' to quit\n")
 
-    qa_chain = build_rag_chain()
+#     qa_chain = build_rag_chain()
 
-    while True:
+#     while True:
 
-        question = input("User: ")
+#         question = input("User: ")
 
-        if question.lower() == "exit":
-            break
+#         if question.lower() == "exit":
+#             break
 
-        result = qa_chain.invoke({
-            "question": question
-        })
+#         result = qa_chain.invoke({
+#             "question": question
+#         })
 
-        answer = result["answer"]
+#         answer = result["answer"]
 
-        print("\nAssistant:", answer)
-        print()
+#         print("\nAssistant:", answer)
+#         print()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
-    
+
+import ollama
+
+response = ollama.chat(
+    model="llama3",
+    messages=[
+        {"role": "user", "content": "Explain black holes simply"}
+    ],
+)
+
+print(response["message"]["content"])
